65eff26bb89bd317bf54992eae253cbfd9010841
max yarish <maxyarish2011@gmail.com>
2024-05-22 21:52:13 +0300

    jsPractic1

---
+++ jspractic1/script.js
@@ -0,0 +1,100 @@
+console.group('1 task');
+console.log('1 task');
+console.groupEnd()
+
+/*1 Напишіть програму, яка запитує у користувача довжину та ширину прямокутника, а потім обчислює та виводить площу і периметр. Підтвердіть розрахунок за допомогою confirm.
+*/
+/*
+const lenghtRectangl=prompt('введіть довжину прямоутника');
+const widthReactangl =prompt('введіть ширину прямокутника');
+if (confirm('ви хочете розрахувати площу і периметр прямокутника')) {
+    const perimetrRectangler=(Number(lenghtRectangl)+Number(widthReactangl))*2;
+    const areRectangl=(widthReactangl*lenghtRectangl);
+    alert('довжина прямокутника= '+lenghtRectangl+'\n'+
+    'ширина прямокутника= '+widthReactangl +'\n'+
+    'площа прямокутник = '+areRectangl+'\n'+
+    'периметр трикутника='+perimetrRectangler
+    );
+} else {
+    alert('OK')
+}*/
+/*
+console.group('2 task');
+console.log("2 task");
+console.log(Math.PI);
+console.log('2**2');
+console.groupEnd('---');
+*/
+/*
+console.group('3 task');
+console.log("3 task");
+console.log(Math.PI);
+console.log('2**2');
+console.groupEnd();
+
+if (confirm('вивести об\'єм ребра кута?')) {
+    const heightRebra=prompt('введіть довжину ребра')
+const summaObiema=Number(heightRebra)**3
+alert(summaObiema)
+} else {
+
+}
+*/
+/*
+console.group('4 task');
+console.log("4 task");
+console.log(Math.PI);
+console.log('2**2');
+console.groupEnd();
+if (confirm) {
+    const radiusOsn=prompt('введіть основу радіуса')
+    const heightCilindra=prompt('введіть висоту циліндра')
+    const summaCilindra=Math.PI*radiusOsn*radiusOsn*heightCilindra;
+    alert(summaCilindra)
+} else {
+}
+*/
+/*
+console.group('5 task');
+console.log("5 task");
+console.log(Math.PI);
+console.log('2**2');
+console.groupEnd();
+
+if (confirm('хочете взнати як привітатися з англійської мови')) {
+    const zvatUsera=prompt('what are your name')
+    alert('hi '+zvatUsera)
+} else {
+    const userName=prompt('як тебе звати')
+    alert('Привіт '+userName)
+}
+*/
+/*
+console.group('6 task');
+console.log("6 task");
+console.log(Math.PI);
+console.log('2**2');
+console.groupEnd();
+
+if (confirm) {
+    const summaOsn=prompt('введіть основну сумму')
+    const chayowi=prompt('введіть % чайових')
+    const vivodnaSumma=(summaOsn)/(chayowi)
+    alert(' чайові на сумму'+vivodnaSumma)
+} else {
+}
+*/
+/*
+console.group('7 task');
+console.log("7 task");
+console.log(Math.PI);
+console.log('2**2');
+console.groupEnd(---);
+*/
+/*
+console.group('8 task');
+console.log("8 task");
+console.log(Math.PI);
+console.log('2**2');
+console.groupEnd(---);
+*/
